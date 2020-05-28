b1f481abee62cf32955da28675f9c97407a70bae
ivan <ivan.tembe@hotmail.com>
2020-05-28 10:27:23 +0200

    Updates

---
+++ precache-manifest.d80eb22653121292fac80affc2f7381f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4d2ee553a9c6f62004cb9b3ca90534",
+    "url": "/window-configurator/index.html"
+  },
+  {
+    "revision": "01c9605f15a2141f682d",
+    "url": "/window-configurator/static/css/2.a4749a00.chunk.css"
+  },
+  {
+    "revision": "d986eb1cf1c83865aefb",
+    "url": "/window-configurator/static/css/main.f33be5f5.chunk.css"
+  },
+  {
+    "revision": "01c9605f15a2141f682d",
+    "url": "/window-configurator/static/js/2.215b0fe3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/window-configurator/static/js/2.215b0fe3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d986eb1cf1c83865aefb",
+    "url": "/window-configurator/static/js/main.b4f1eb10.chunk.js"
+  },
+  {
+    "revision": "64b12c02697a424daf22",
+    "url": "/window-configurator/static/js/runtime-main.e3b721ad.js"
+  }
+]);
